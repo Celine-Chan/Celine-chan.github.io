b9431532fef0cb02df7497cd0b6c6354241a7887
Celine_lhp5 <celine.louvel.76@outlook.fr>
2020-11-17 16:17:18 +0100

    modif ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,91 @@
-const showNumberOne = document.getElementById('firstNumber')
+const showNumberOne = document.getElementById("firstNumber");
+const showNumberTwo = document.getElementById("secondNumber");
+let calculString = "";
 
-function nineNumber() {
-    showNumberOne.innerHTML += "9";
-};
+function addNumber(number) {
+    showNumberOne.innerHTML += number;
+}
 
-function eightNumber() {
-    showNumberOne.innerHTML += "8";
-};
 
-function sevenNumber() {
-    showNumberOne.innerHTML += "7";
-};
 
-function sixNumber() {
-    showNumberOne.innerHTML += "6";
-};
 
-function fiveNumber() {
-    showNumberOne.innerHTML += "5";
-};
 
-function fourNumber() {
-    showNumberOne.innerHTML += "4";
-};
 
-function threeNumber() {
-    showNumberOne.innerHTML += "3";
-};
 
-function twoNumber() {
-    showNumberOne.innerHTML += "2";
-};
 
-function oneNumber() {
-    showNumberOne.innerHTML += "1";
-};
 
-function zeroNumber() {
-    showNumberOne.innerHTML += "0";
-};
 
-function addNumber() {
-    showNumberOne.innerHTML += "+";
-};
 
-function multiplicateNumber() {
-    showNumberOne.innerHTML += "x";
-};
 
-function minusNumber() {
-    showNumberOne.innerHTML += "-";
-};
 
-function splitNumber() {
-    showNumberOne.innerHTML += "/";
-};
 
-function dotNumber() {
-    showNumberOne.innerHTML += ".";
-};
+
+
+
+
+
+
+// function nineNumber() {
+//     showNumberOne.innerHTML += "9";
+// };
+
+// function eightNumber() {
+//     showNumberOne.innerHTML += "8";
+// };
+
+// function sevenNumber() {
+//     showNumberOne.innerHTML += "7";
+// };
+
+// function sixNumber() {
+//     showNumberOne.innerHTML += "6";
+// };
+
+// function fiveNumber() {
+//     showNumberOne.innerHTML += "5";
+// };
+
+// function fourNumber() {
+//     showNumberOne.innerHTML += "4";
+// };
+
+// function threeNumber() {
+//     showNumberOne.innerHTML += "3";
+// };
+
+// function twoNumber() {
+//     showNumberOne.innerHTML += "2";
+// };
+
+// function oneNumber() {
+//     showNumberOne.innerHTML += "1";
+// };
+
+// function zeroNumber() {
+//     showNumberOne.innerHTML += "0";
+// };
+
+// function addNumber() {
+//     showNumberOne.innerHTML += "+";
+//     calculString = "+";
+// };
+
+// function multiplicateNumber() {
+//     showNumberOne.innerHTML += "x";
+// };
+
+// function minusNumber() {
+//     showNumberOne.innerHTML += "-";
+// };
+
+// function splitNumber() {
+//     showNumberOne.innerHTML += "/";
+// };
+
+// function dotNumber() {
+//     showNumberOne.innerHTML += ".";
+// };
+
 
 
 
