1330b60e4acffdb0ce586ca9c8566cfc5ffe8bd2
Celine_lhp5 <celine.louvel.76@outlook.fr>
2020-11-17 15:47:30 +0100

    modif ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,67 @@
-let firstNumber = document.getElementById("nine");
-let secondNumber = document.getElementById("eight");
+const showNumberOne = document.getElementById('firstNumber')
 
 function nineNumber() {
-    
-}
+    showNumberOne.innerHTML += "9";
+};
+
+function eightNumber() {
+    showNumberOne.innerHTML += "8";
+};
+
+function sevenNumber() {
+    showNumberOne.innerHTML += "7";
+};
+
+function sixNumber() {
+    showNumberOne.innerHTML += "6";
+};
+
+function fiveNumber() {
+    showNumberOne.innerHTML += "5";
+};
+
+function fourNumber() {
+    showNumberOne.innerHTML += "4";
+};
+
+function threeNumber() {
+    showNumberOne.innerHTML += "3";
+};
+
+function twoNumber() {
+    showNumberOne.innerHTML += "2";
+};
+
+function oneNumber() {
+    showNumberOne.innerHTML += "1";
+};
+
+function zeroNumber() {
+    showNumberOne.innerHTML += "0";
+};
+
+function addNumber() {
+    showNumberOne.innerHTML += "+";
+};
+
+
+function multiplicateNumber() {
+    showNumberOne.innerHTML += "x";
+};
+
+function minusNumber() {
+    showNumberOne.innerHTML += "-";
+};
+
+function splitNumber() {
+    showNumberOne.innerHTML += "/";
+};
+
+function dotNumber() {
+    showNumberOne.innerHTML += ".";
+};
+
+
 
 
 
